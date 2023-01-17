10fcab61d734d3bfa2ae579aa40a973d531e4f41
cas-sav <94647032+cas-sav@users.noreply.github.com>
2023-01-16 22:12:16 -0500

    Mongoose update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-mongoose.connect( 'process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('strictQuery', true);
 
 app.use(bodyParser.json());
