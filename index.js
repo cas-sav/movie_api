3ab453d82bad9f538466d6874ad9de249523ce62
cas-sav <94647032+cas-sav@users.noreply.github.com>
2023-02-13 19:00:31 -0500

    removed jwt from users

--- index.js
+++ index.js
@@ -351,7 +351,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 
 
 // READ (users, all)
-app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/users', (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
